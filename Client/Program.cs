2f7bab2587c03313b899d8397fcb32a20fb29ed6
snezhkalesha@gmail.com <snezhkalesha@gmail.com>
2022-11-11 08:45:27 +0300

    +

---
+++ Client/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Connect(new IPEndPoint(IPAddress.Loopback, 8000));
+
+            var matrix_1 = new float[,]
+            {
+                { 1, 2, 3, 4 },
+                { 1, 2, 3, 4 },
+                { 1, 2, 3, 4 },
+            };
+
+            var matrix_2 = new float[,]
+            {
+                { 1, 2, 3, 4, 5 },
+                { 1, 2, 3, 4, 5 },
+                { 1, 2, 3, 4, 5 },
+                { 1, 2, 3, 4, 5 },
+            };
+
+            socket.Send(BitConverter.GetBytes(3));
+            socket.Send(BitConverter.GetBytes(4));
+            socket.Send(BitConverter.GetBytes(4));
+            socket.Send(BitConverter.GetBytes(5));
+
+            foreach (var element in matrix_1)
+            {
+                socket.Send(BitConverter.GetBytes(element));
+            }
+
+            foreach (var element in matrix_2)
+            {
+                socket.Send(BitConverter.GetBytes(element));
+            }
+
+            var resultMatrix = new float[3, 5];
+
+            for (var rowIndex = 0; rowIndex < 3; ++rowIndex)
+            {
+                for (var columnIndex = 0; columnIndex < 5; ++columnIndex)
+                {
+                    var buffer = new byte[4];
+
+                    socket.Receive(buffer);
+
+                    resultMatrix[rowIndex, columnIndex] = BitConverter.ToSingle(buffer);
+
+                    Console.Write("{0, -5}", resultMatrix[rowIndex, columnIndex]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ lab4/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace lab4
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"Configuring server with AddressFamily.InterNetwork, SocketType.Stream and ProtocolType.Tcp...");
+            var serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Done!");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Loopback, 8000));
+            Console.WriteLine($"Listening port 8000...");
+            serverSocket.Listen();
+            int i = 1;
+            while (true)
+            {
+                var clientSocket = serverSocket.Accept();
+                Console.WriteLine($"Accepted client #{i++}!");
+                ThreadPool.QueueUserWorkItem(ClientCallback, clientSocket);
+            }
+        }
+
+        private static void ClientCallback(object? state)
+        {
+            var clientSocket = (Socket)state!;
+
+            var rowCount_1 = clientSocket.ReceiveInt32();
+            var columnCount_1 = clientSocket.ReceiveInt32();
+            var rowCount_2 = clientSocket.ReceiveInt32();
+            var columnCount_2 = clientSocket.ReceiveInt32();
+
+            var matrix_1 = new float[rowCount_1, columnCount_1];
+            var matrix_2 = new float[rowCount_2, columnCount_2];
+
+            for (var rowIndex = 0; rowIndex < rowCount_1; ++rowIndex)
+            {
+                for (var columnIndex = 0; columnIndex < columnCount_1; ++columnIndex)
+                {
+                    matrix_1[rowIndex, columnIndex] = clientSocket.ReceiveSingle();
+                }
+            }
+
+            for (var rowIndex = 0; rowIndex < rowCount_2; ++rowIndex)
+            {
+                for (var columnIndex = 0; columnIndex < columnCount_2; ++columnIndex)
+                {
+                    matrix_2[rowIndex, columnIndex] = clientSocket.ReceiveSingle();
+                }
+            }
+
+            var resultMatrix = new float[rowCount_1, columnCount_2];
+
+            var n = rowCount_2;
+
+            for (var rowIndex = 0; rowIndex < rowCount_1; ++rowIndex)
+            {
+                for (var columnIndex = 0; columnIndex < columnCount_2; ++columnIndex)
+                {
+                    var value = default(float);
+
+                    for (var i = 0; i < n; ++i)
+                    {
+                        value += matrix_1[rowIndex, i] * matrix_2[i, columnIndex];
+                    }
+
+                    resultMatrix[rowIndex, columnIndex] = value;
+                }
+            }
+
+            for (var rowIndex = 0; rowIndex < rowCount_1; ++rowIndex)
+            {
+                for (var columnIndex = 0; columnIndex < columnCount_2; ++columnIndex)
+                {
+                    clientSocket.Send(BitConverter.GetBytes(resultMatrix[rowIndex, columnIndex]));
+                }
+            }
+            clientSocket.Close();
+        }
+    }
+
+    internal static class SocketExtensions
+    {
+        public static int ReceiveInt32(this Socket socket)
+        {
+            var buffer = new byte[4];
+
+            socket.Receive(buffer);
+
+            return BitConverter.ToInt32(buffer);
+        }
+
+        public static float ReceiveSingle(this Socket socket)
+        {
+            var buffer = new byte[4];
+
+            socket.Receive(buffer);
+
+            return BitConverter.ToSingle(buffer);
+        }
+    }
+}
